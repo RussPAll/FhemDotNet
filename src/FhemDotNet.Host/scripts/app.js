5c370593ca3b02f34532870f14740360a5261809
Russell Allen <russell.allen@drllimited.co.uk>
2013-02-05 20:06:09 +0000

    Started work on slider directive

--- src/FhemDotNet.Host/scripts/app.js
+++ src/FhemDotNet.Host/scripts/app.js
@@ -1,1 +1,13 @@
-﻿var fhemDotNet = angular.module('fhemDotNet', []);
+﻿var fhemDotNet = angular.module('fhemDotNet', [])
+    .directive('slider', function () {
+        return {
+            restrict: 'A',
+            link: function(scope, element, attrs) {
+                element.slider({
+                    min: 6,
+                    max: 29,
+                    value: 10
+                });
+            }
+        };
+    });
