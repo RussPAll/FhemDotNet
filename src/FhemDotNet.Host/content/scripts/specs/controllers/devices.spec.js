19323247d1d13ff6a2343c314df2c0e7f5c55ea5
Russell Allen <russell.allen@drllimited.co.uk>
2013-03-10 20:55:30 +0000

    Removed in-project Jasmine runner
    Using ReSharper runner for now, will introduce something else for
    build-time testing e.g. Testacular, JsTestDriver, etc

--- src/FhemDotNet.Host/content/scripts/specs/controllers/devices.spec.js
+++ src/FhemDotNet.Host/content/scripts/specs/controllers/devices.spec.js
@@ -1,9 +1,17 @@
-﻿describe("FhemDotNet", function () {
+﻿/// <reference path="../../angular/angular.js"/>
+/// <reference path="../../angular/angular-mocks.js"/>
+/// <reference path="../../app.js"/>
+/// <reference path="../../controllers/devices.js"/>
+
+describe("FhemDotNet", function () {
     describe("Controllers", function () {
         beforeEach(module('fhemDotNet'));
+        it('should execute', function () {
+            expect(true).toBe(true);
+        });
         describe("ThermostatListCtrl", function () {
-            it("should download device data",
-                inject(function (_$httpBackend_, $rootScope, $controller) {
+            it('should download device data', function() {
+                inject(function(_$httpBackend_, $rootScope, $controller) {
                     _$httpBackend_.whenGET('/devices').
                         respond([{ one: 'test' }, { one: 'test'}]);
 
@@ -12,7 +20,7 @@
                     _$httpBackend_.flush();
                     expect(scope.devices.length).toBe(2);
                 })
-            );
+            });
         });
     });
 });
