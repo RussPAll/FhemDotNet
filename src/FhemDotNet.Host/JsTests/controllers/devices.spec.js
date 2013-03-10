4b7409a25ae7b6082263d199bb4249972bd0cd54
Russell Allen <russell.allen@drllimited.co.uk>
2013-03-10 17:22:53 +0000

    Continued work on unit and javascript testing

---
+++ src/FhemDotNet.Host/JsTests/controllers/devices.spec.js
@@ -0,0 +1,18 @@
+﻿describe("FhemDotNet", function () {
+    describe("Controllers", function () {
+        beforeEach(module('fhemDotNet'));
+        describe("ThermostatListCtrl", function () {
+            it("should download device data",
+                inject(function (_$httpBackend_, $rootScope, $controller) {
+                    _$httpBackend_.whenGET('/devices').
+                        respond([{ one: 'test' }, { one: 'test'}]);
+
+                    var scope = $rootScope.$new(),
+                        ctrl = $controller("ThermostatListCtrl", { $scope: scope });
+                    _$httpBackend_.flush();
+                    expect(scope.devices.length).toBe(2);
+                })
+            );
+        });
+    });
+});
