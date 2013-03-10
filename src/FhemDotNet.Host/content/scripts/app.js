da7334e7079286b401553aa5f8a1d0d7238125d0
Russell Allen <russell.allen@drllimited.co.uk>
2013-03-10 13:50:06 +0000

    Added Twitter Bootstrap

--- src/FhemDotNet.Host/content/scripts/app.js
+++ src/FhemDotNet.Host/content/scripts/app.js
@@ -1,24 +1,24 @@
 ﻿angular.module('ui.config', []).value('ui.config', { });
 var fhemDotNet = angular.module('fhemDotNet', ['ui.config'])
-    .directive('uiSlider', ['ui.config', function (uiConfig) {
+    .directive('uiSlider', ['ui.config', function(uiConfig) {
         'use strict';
         var options;
-        options = {};
+        options = { };
         if (angular.isObject(uiConfig.slider)) {
             angular.extend(options, uiConfig.slider);
         }
         return {
             require: '?ngModel',
-            link: function (scope, element, attrs, controller) {
-                var getOptions = function () {
-                    return angular.extend({}, uiConfig.slider, scope.$eval(attrs.uiSlider));
+            link: function(scope, element, attrs, controller) {
+                var getOptions = function() {
+                    return angular.extend({ }, uiConfig.slider, scope.$eval(attrs.uiSlider));
                 };
-                var initSliderWidget = function () {
+                var initSliderWidget = function() {
                     var opts = getOptions();
 
                     if (controller) {
-                        var updateModel = function () {
-                            scope.$apply(function () {
+                        var updateModel = function() {
+                            scope.$apply(function() {
                                 var value = element.slider("value");
                                 controller.$setViewValue(value);
                                 element.blur();
@@ -28,9 +28,9 @@ var fhemDotNet = angular.module('fhemDotNet', ['ui.config'])
                         if (opts.onSlide) {
                             // Caller has specified onSelect, so call this as well as updating the model
                             var userOnSlide = opts.onSlide;
-                            opts.slide = function (event, ui) {
+                            opts.slide = function(event, ui) {
                                 updateModel();
-                                scope.$apply(function () {
+                                scope.$apply(function() {
                                     userOnSlide(event, ui);
                                 });
                             };
@@ -42,20 +42,17 @@ var fhemDotNet = angular.module('fhemDotNet', ['ui.config'])
                         if (opts.onChange) {
                             // Caller has specified onSelect, so call this as well as updating the model
                             var userOnChange = opts.onChange;
-                            opts.change = function (event, ui) {
+                            opts.change = function(event, ui) {
                                 updateModel();
-                                scope.$apply(function () {
+                                scope.$apply(function() {
                                     userOnChange(event, ui);
                                 });
                             };
                         }
 
                         // Update the slider when the model changes
-                        controller.$render = function () {
+                        controller.$render = function() {
                             var value = controller.$viewValue;
-                            if (angular.isDefined(value) && value !== null && !angular.isNumber(value)) {
-                                throw new Error('ng-Model value must be a number object - currently it is a ' + typeof value);
-                            }
                             element.slider("value", value);
                         };
                     }
@@ -70,4 +67,19 @@ var fhemDotNet = angular.module('fhemDotNet', ['ui.config'])
                 scope.$watch(getOptions, initSliderWidget, true);
             }
         };
-    } ]);
+    }]).directive('ngModelOnblur', function() {
+        return {
+            restrict: 'A',
+            require: 'ngModel',
+            link: function(scope, elm, attr, ngModelCtrl) {
+                if (attr.type === 'radio' || attr.type === 'checkbox') return;
+
+                elm.unbind('input').unbind('keydown').unbind('change');
+                elm.bind('blur', function() {
+                    scope.$apply(function() {
+                        ngModelCtrl.$setViewValue(elm.val());
+                    });
+                });
+            }
+        };
+    });
